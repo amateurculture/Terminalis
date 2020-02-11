94b0b017669aa5bd4c8cff0295bb5ced285c37a0
Fiona Schultz <amateurculture@gmail.com>
2020-02-11 09:05:28 -0500

    Added some mirror button change clothing functionality.

---
+++ Assets/ChangeClothing.cs
@@ -0,0 +1,40 @@
+﻿using UMA;
+using UMA.CharacterSystem;
+using UnityEngine;
+
+public class ChangeClothing : MonoBehaviour
+{
+    public DynamicCharacterAvatar avatar;
+    public UMATextRecipe recipe;
+
+    public void Add()
+    {
+        avatar.ClearSlot(recipe.wardrobeSlot);
+        avatar.SetSlot(recipe);
+        avatar.BuildCharacter();
+    }
+
+    public void Toggle()
+    {
+        // TODO lol... it never does what I need it to do... 
+    }
+
+    /*
+    void AddClothing(DynamicCharacterAvatar avatar, UMATextRecipe recipe)
+    {
+        if (recipe != null)
+        {
+            avatar.SetSlot(recipe);
+            avatar.BuildCharacter();
+        }
+    }
+    void RemoveClothing(DynamicCharacterAvatar avatar, string recipe)
+    {
+        if (recipe != null)
+        {
+            avatar.ClearSlot(recipe);
+            avatar.BuildCharacter();
+        }
+    }
+    */
+}
