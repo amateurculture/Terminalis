d21ddcd3508ded426a6d3f957592250a807d0d63
Fiona Schultz <amateurculture@gmail.com>
2020-02-18 21:23:14 -0500

    Added lens flares and fixed lighting.

--- Assets/TimeController.cs
+++ Assets/TimeController.cs
@@ -2,73 +2,74 @@
 
 public class TimeController : MonoBehaviour
 {
-    public float DayCounter; //Counts the amount of days that have passed
-
-    public float DayTimeLength; //Sets how long each day lasts for. Higher values make the day go faster
-    public float NightTimeLength; //Sets how long each night lasts for. Higher values make the night go faster
-
-    public bool isDayTime; //On off Switch for Day time. Can use this so that things have different attributes during the day
-    public bool isNightTime; //On off Switch for Night time. Can use this so that things have different attributes during the Night
-
-    private bool EndOfDay; //Variable used to isolate the point in time when the next day will start 
-    bool nextDay; //A check to see if it's time to move onto the next day.
-
+    public float DayCounter;
+    public float startHour = 6;
+    public float secondsInHour = 10f;
+    public float time;
     public Gradient sunLight;
     public Gradient ambientLight;
     public Gradient fogColor;
-    Light sun;
     public int frameSkip = 50;
     public ReflectionProbe reflectionProbe;
 
+    bool isDayTime = true; 
+    bool isNightTime; 
+    bool EndOfDay;
+    bool nextDay;
+    float secondsRemainingInMinute = 0;
+    Light sun;
+    
     private void Start()
     {
         sun = GetComponent<Light>();
+
+        transform.eulerAngles = Vector3.zero;
+        var angles = transform.eulerAngles;
+        angles.y = 45;
+        angles.x = 15 * (startHour-6);
+        transform.Rotate(angles);
+        time = Time.time;
     }
 
     void Update()
     {
-        if (Time.frameCount % frameSkip == 0)
+        if (Time.frameCount % 200 == 0)
         {
-            //Grabs the rotation of the Directional Light. These rotations are the period of time that there is light, creating the Day cycle
+            reflectionProbe.RenderProbe();
+        }
+        else if (Time.time > secondsRemainingInMinute)
+        {
+            secondsRemainingInMinute = Time.time + (secondsInHour / 60);
+
             if (transform.eulerAngles.x > 0 && transform.eulerAngles.x < 180)
             {
                 isNightTime = false;
                 isDayTime = true;
             }
 
-            //Grabs the rotation of the Directional Light. These rotations are the period of time that there is darkness, creating the Night cycle
             else if (transform.eulerAngles.x > 180 && transform.eulerAngles.x < 360)
             {
                 isDayTime = false;
                 isNightTime = true;
             }
 
-            //While the Directional Light is in the Day Cycle it'll rotate the camera on it's x axis to progress time at the speed of the Day Cycle.
             if (isDayTime)
-                transform.Rotate(frameSkip * DayTimeLength * Time.deltaTime, 0, 0);
+                transform.Rotate(.25f, 0, 0);
 
-            //While the Directional Light is in the Day Cycle it'll rotate the camera on it's x axis to progress time at the speed of the Night Cycle.
             if (isNightTime)
-                transform.Rotate(frameSkip * NightTimeLength * Time.deltaTime, 0, 0);
+                transform.Rotate(.25f, 0, 0);
 
-
-            //Change fog color based on time of day
             RenderSettings.fogColor = fogColor.Evaluate(transform.eulerAngles.x / 360);
             RenderSettings.ambientLight = ambientLight.Evaluate(transform.eulerAngles.x / 360);
             sun.intensity = sunLight.Evaluate(transform.eulerAngles.x / 360).grayscale;
 
-            //Update reflection probe
-            //reflectionProbe.backgroundColor = RenderSettings.fogColor;
-            reflectionProbe.RenderProbe();
 
-            //Function for increasing the DayCounter Variable
             IncreaseNumberOfDaysSurvived();
         }
     }
 
     void IncreaseNumberOfDaysSurvived()
     {
-        //Finds rough rotation in which we want to end a full day (midnight).
         if (transform.eulerAngles.x > 270 && transform.eulerAngles.x < 280)
         {
             EndOfDay = true;
@@ -79,7 +80,6 @@ public class TimeController : MonoBehaviour
             EndOfDay = false;
         }
 
-        //If it's the end of the day but not the next day yet then we make the transition to the next day and increase the DayCounter to the next day.
         if (EndOfDay && !nextDay)
         {
             nextDay = true;
